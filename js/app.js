cafd0e1e9bde1b8bece3e25d2ba25583e285a5ef
stphngrtz <stephan.goertz@gmail.com>
2015-09-13 14:04:24 +0200

    level 4

--- js/app.js
+++ js/app.js
@@ -3,27 +3,65 @@
   app.controller("StoreController", function() {
     this.products = gems;
   });
-  app.controller("TabController", function() {
-    this.tab = 1;
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab) {
-      return this.tab === checkTab;
-    };
-  });
-  app.controller("GalleryController", function() {
-    this.current = 0;
-    this.setCurrent = function(val) {
-      this.current = val || 0;
-    };
-  });
   app.controller("ReviewController", function() {
     this.review = {};
     this.addReview = function(product) {
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
+    };
+  });
+  app.directive("productTitle", function() {
+    return {
+      restrict: "E", // E = Element, A = Attribute
+      templateUrl: "product-title.html"
+    };
+  });
+  app.directive("productGallery", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-gallery.html",
+      controller: function() {
+        this.current = 0;
+        this.setCurrent = function(val) {
+          this.current = val || 0;
+        };
+      },
+      controllerAs: "gallery"
+    };
+  });
+  app.directive("productTabs", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-tabs.html",
+      controller: function() {
+        this.tab = 1;
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: "tab"
+    };
+  });
+  app.directive("productDescription", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-description.html"
+    };
+  });
+  app.directive("productSpecifications", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-specifications.html"
+    };
+  });
+  app.directive("productReviews", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-reviews.html"
     };
   });
   var gems = [{
@@ -66,7 +104,8 @@
       "images/gem-01.gif",
       "images/gem-03.gif",
       "images/gem-04.gif"
-    ]
+    ],
+    reviews: []
   },{
     name: "Zircon",
     description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
@@ -81,6 +120,7 @@
       "images/gem-06.gif",
       "images/gem-07.gif",
       "images/gem-09.gif"
-    ]
+    ],
+    reviews: []
   }];
 })();
